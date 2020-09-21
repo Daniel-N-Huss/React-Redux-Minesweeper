83d79e368281d548d4386b8b4e9e54fd90f14b24
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-21 16:56:20 -0600

    skeleton of game data generator

---
+++ src/helpers/gameData/tileGenerator.js
@@ -0,0 +1,13 @@
+const boardwidth = 16;
+const mineCount = 40;
+
+
+const generateTiles = function (boardWidth, mineCount) {
+
+  const boardSize = boardWidth ** 2;
+
+  let gameData = [];
+
+  return gameData;
+
+}
