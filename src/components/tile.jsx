102b892be67a6a9edec4d733e5b62b95b44fb9a0
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-23 00:46:51 -0600

    basic onClick reveal tile content

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -3,12 +3,12 @@ import './Tile.scss'
 
 const Tile = function (props) {
 
-  const {mine, dangerRating} = props;
+  const {mine, danger} = props;
   const [revealed, setRevealed] = useState(props.revealed)
 
 
   return <td className="tile" onClick={() => setRevealed(true)}>
-    {revealed ? dangerRating : ' '}
+    {revealed ? danger : ' '}
   </td>;
 };
 
