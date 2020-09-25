c974b831ab85c981557260818374897a43b31e4a
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-24 20:16:02 -0600

    gameBoard state now managed by redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.scss';
 import Board from './components/Board';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const gameBoard = useSelector(state => state.gameBoard)
   return (
     <div className='App'>
       <header className='App-header'>
         <p>A Minesweeper Game Will Appear</p>
         <table>
           <tbody>
-            <Board gameData="BRING STORE IN HERE"/>
+            <Board gameData={gameBoard} />
           </tbody>
         </table>
       </header>
