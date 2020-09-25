8db038ca0c9ab54e98392170a07180a08e9cd7bf
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-25 17:33:27 -0600

    recursively reveal tiles with 0 adjacent mines

--- src/helpers/redux/revealSafeTiles.js
+++ src/helpers/redux/revealSafeTiles.js
@@ -1,16 +1,22 @@
 // Again my implementation is utilizing nested arrays loops to identify which other tiles are neighbors
 // to reveal them when the currently clicked tile has a 0 danger rating.
 // I'm uncomfortable with the implementation, but there is a hard limit - only a max of 8 neighbors exist for each tile,
-// so the performance impacts of these nested loops is small. Finding a better data structure is a must-do in larger scale apps.
+// so the performance impacts of these nested loops is small though gets more impactful because of the recursion. 
+//Finding a better data structure is a must-do in larger scale apps.
 
-const revealSafeTiles = function (adjacentTileIdsArray, shallowStateGameBoard) {
+const revealSafeTiles = function (adjacentTileIDsArray, shallowStateGameBoard) {
   const shallowStateKeys = Object.keys(shallowStateGameBoard);
 
-  adjacentTileIdsArray.forEach((id) => {
+  adjacentTileIDsArray.forEach((id) => {
     for (const key of shallowStateKeys) {
       let foundTile = shallowStateGameBoard[key].find((tile) => tile.id === id);
 
-      if (foundTile) foundTile.revealed = true;
+      if (foundTile && !foundTile.revealed) {
+        foundTile.revealed = true;
+        if (!foundTile.danger) {
+          revealSafeTiles(foundTile.adjacentTileIDs, shallowStateGameBoard)
+        }
+      }
     }
   });
 };
