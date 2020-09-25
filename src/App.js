220cae5336a5356fbf2bac2fad9240d3143d9b5e
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-24 20:28:43 -0600

    button now dispatches a board reset

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.scss';
 import Board from './components/Board';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {reset} from './helpers/redux/actions'
 
 function App() {
   const gameBoard = useSelector(state => state.gameBoard)
-  return (
+  const dispatch = useDispatch();
+  return ( 
     <div className='App'>
       <header className='App-header'>
         <p>A Minesweeper Game Will Appear</p>
+        <button onClick={() => dispatch(reset())}>Reset The Game!</button>
         <table>
           <tbody>
             <Board gameData={gameBoard} />
--- src/helpers/redux/actions.js
+++ src/helpers/redux/actions.js
@@ -1,5 +1,7 @@
-import { START_GAME, GAME_OVER, SET_VICTORY, RESET_BOARD } from "../actionTypes";
+import { START_GAME, GAME_OVER, SET_VICTORY, RESET_BOARD } from "./actionTypes";
 
-export const revealTile = () => ({
-  
-}) 
+export const reset = function () {
+  return {
+    type: "RESET_BOARD"
+  }
+}
