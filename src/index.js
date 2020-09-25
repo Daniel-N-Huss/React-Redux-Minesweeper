315c3ad868679007397c4c978e8213bd8c64b867
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-24 20:34:42 -0600

    move store init out of app file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-//import store from './helpers/redux/store'
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './helpers/redux/store'
 
-import { createStore } from 'redux';
-import rootReducer from './helpers/redux/reducers';
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
   <React.StrictMode>
