838a7777c3c058bb603a7ec1a20df853603e6d8e
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-25 17:40:45 -0600

    remove unused reducer post refactor

--- src/helpers/redux/reducers/index.js
+++ src/helpers/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import gameStateReducer from './gameState';
-import revealReducer from './revealed';
 import gameBoardReducer from './gameBoard';
 
 export default combineReducers({
   gameState: gameStateReducer,
-  reveal: revealReducer,
   gameBoard: gameBoardReducer,
 });
