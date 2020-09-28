3817db5e2901541bda16d660134a502473f6a32e
Daniel-n-huss <daniel.n.huss@gmail.com>
2020-09-28 10:49:51 -0600

    flag count now connected to store

--- src/components/StatDisplay.jsx
+++ src/components/StatDisplay.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import flagIcon from './flagIcon.png';
 import stopwatchIcon from './stopwatch.png'
 import './StatDisplay.scss';
 
 const StatDisplay = function () {
+
+  //let timer = useSelector((state) => {})
+  let flagCount = useSelector((state) => state.flagCount);
+
   return (
     <span className='stats'>
       <div>
         <img src={flagIcon} alt='Flag Count' />
-        <p>40</p>
+        <p>{flagCount}</p>
       </div>
       <div>
         <img src={stopwatchIcon} alt='Timer' />
